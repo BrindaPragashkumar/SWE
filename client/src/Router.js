94829383058b0031e0e07f320a6ad5d7e9c4293c
Manjit Singh <93484002+Sen-Is-Dead@users.noreply.github.com>
2023-04-07 15:04:52 +0100

    Update Router.js

--- client/src/Router.js
+++ client/src/Router.js
@@ -4,8 +4,10 @@ import LandingPage from './pages/LandingPage';
 import NotFound from './pages/NotFound';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import Sidebar from './components/sidebar';
+import Sidebar from './components/Sidebar';
 import httpClient from './httpClient';
+import AddPropertyPage from './pages/AddPropertyPage'
+import PropertyDetailsPage from './pages/PropertyDetailsPage';
 
 const Router = () => {
   const [user, setUser] = useState(null);
@@ -30,6 +32,8 @@ const Router = () => {
             <Route path="/" element={<LandingPage user={user} setUser={setUser} />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/add-property" element={<AddPropertyPage user={user} />} />
+            <Route path="/property/:propertyId" element={<PropertyDetailsPage />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
