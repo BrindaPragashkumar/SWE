94283550025490ecd380157aeda7ee53c4d3a6eb
Manjit Singh <93484002+Sen-Is-Dead@users.noreply.github.com>
2023-04-07 14:33:57 +0100

    Update config.py

--- server/config.py
+++ server/config.py
@@ -9,9 +9,12 @@ class ApplicationConfig:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = r"sqlite:///./new_db.sqlite"
+    SQLALCHEMY_DATABASE_URI = r"sqlite:///./db2.sqlite"
 
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
     SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
