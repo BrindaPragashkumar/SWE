fbb983bdbd56a86a9336e159ea0f7d0a4904222e
Manjit Singh <93484002+Sen-Is-Dead@users.noreply.github.com>
2023-04-07 14:33:42 +0100

    Update models.py

--- server/models.py
+++ server/models.py
@@ -1,10 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from uuid import uuid4
+import random
 
 db = SQLAlchemy()
 
 def get_uuid():
     return uuid4().hex
+
+def get_random_6_digit_number():
+    number = random.randint(100000, 999999)
+    
+    while Property.query.filter_by(id=number).first() is not None:
+        number = random.randint(100000, 999999)
+    
+    return number
 
 class User(db.Model):
     __tablename__ = "users"
@@ -14,3 +23,14 @@ class User(db.Model):
     first_name = db.Column(db.String(32), nullable=False)
     last_name = db.Column(db.String(32), nullable=False)
     role = db.Column(db.String(32), nullable=False)
+    properties = db.relationship('Property', backref='user', lazy=True)
+
+class Property(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, default=get_random_6_digit_number)
+    address = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    userId = db.Column(db.String(32), db.ForeignKey('users.id'), nullable=False)
+    photos = db.Column(db.Text)
+    bedrooms = db.Column(db.Integer, nullable=False)
+    bathrooms = db.Column(db.Integer, nullable=False)
