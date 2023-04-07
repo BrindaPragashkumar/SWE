7cec653e3a98a60a3d6dfa47adf9e2134ad92caf
Manjit Singh <93484002+Sen-Is-Dead@users.noreply.github.com>
2023-04-07 14:34:10 +0100

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,9 +1,16 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify, session, request, url_for
+from werkzeug.utils import secure_filename
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_session import Session
 from config import ApplicationConfig
-from models import db, User
+from models import db, User, Property
+import uuid
+import os
+import json
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ApplicationConfig.ALLOWED_EXTENSIONS
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
@@ -84,6 +91,63 @@ def login_user():
 def logout_user():
     session.pop("user_id")
     return "200"
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'image' not in request.files:
+        return 'No image provided', 400
+
+    image = request.files['image']
+    if image.filename == '':
+        return 'No image provided', 400
+
+    if not allowed_file(image.filename):
+        return 'Invalid file type', 400
+
+    filename = secure_filename(image.filename)
+    image_extension = filename.rsplit('.', 1)[1].lower()
+    unique_filename = f"{uuid.uuid4().hex}.{image_extension}"
+    image.save(os.path.join(ApplicationConfig.UPLOAD_FOLDER, unique_filename))
+
+    image_url = url_for('static', filename=f'uploads/{unique_filename}', _external=True)
+    return {'url': image_url}, 200
+
+@app.route('/properties/<property_id>', methods=['GET'])
+def get_property(property_id):
+    property = Property.query.filter_by(id=property_id).first()
+    if property is None:
+        return jsonify({"error": "Property not found"}), 404
+
+    property_data = {
+        'id': property.id,
+        'address': property.address,
+        'description': property.description,
+        'price': property.price,
+        'userId': property.userId,
+        'photos': json.loads(property.photos),
+        'bedrooms': property.bedrooms,
+        'bathrooms': property.bathrooms
+    }
+    return jsonify(property_data)
+
+# app.py
+
+@app.route('/properties', methods=['POST'])
+def add_property():
+    address = request.json["address"]
+    description = request.json["description"]
+    price = request.json["price"]
+    userId = request.json["userId"]
+    photos = request.json["photos"]
+    bedrooms = request.json["bedrooms"]
+    bathrooms = request.json["bathrooms"]
+
+    new_property = Property(address=address, description=description, price=price, userId=userId, photos=photos, bedrooms=bedrooms, bathrooms=bathrooms)
+    db.session.add(new_property)
+    db.session.commit()
+
+    return jsonify({"id": new_property.id, "message": "Property added successfully"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
