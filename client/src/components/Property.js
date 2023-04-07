98dab440370d40cd38df223c2853e4348d0295bf
Manjit Singh <93484002+Sen-Is-Dead@users.noreply.github.com>
2023-04-07 15:08:39 +0100

    Add files via upload

---
+++ client/src/components/Property.js
@@ -0,0 +1,15 @@
+class Property {
+    constructor(id, address, description, price, userId, photos, bedrooms, bathrooms) {
+      this.id = id;
+      this.address = address;
+      this.description = description;
+      this.price = price;
+      this.userId = userId;
+      this.photos = photos;
+      this.bedrooms = bedrooms;
+      this.bathrooms = bathrooms;
+    }
+  }
+  
+  export default Property;
+  
