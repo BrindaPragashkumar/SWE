e725fc0e5d30e639b7027c49ccc5a2ba77ab628c
Manjit Singh <93484002+Sen-Is-Dead@users.noreply.github.com>
2023-04-13 00:26:54 +0100

    Add files via upload

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { UserShape } from '../Types';
-import httpClient from '../httpClient'
+import httpClient from '../httpClient';
 import './Sidebar.css';
-import icon from './DefaultIcon.jpg'
+import icon from './DefaultIcon.jpg';
 
 const Sidebar = ({ user }) => {
-
   const logoutUser = async () => {
     await httpClient.post('//localhost:5000/logout');
-    window.location.href = "/"
-  }  
+    window.location.href = '/';
+  };
+
+  const isLandlord = user.role.toLowerCase() === 'landlord';
 
   return (
     <div className="sidebar-container">
       <div className="profile-container">
         <div className="profile-picture" style={{ backgroundImage: `url(${icon})` }}></div>
         <div className="user-details">
-          <p>{user.first_name} {user.last_name}</p>
+          <p>
+            {user.first_name} {user.last_name}
+          </p>
           <p>{user.role.charAt(0).toUpperCase() + user.role.slice(1)}</p>
         </div>
       </div>
       <div className="sidebar-options">
-      <a href="/">Home</a>
+        <a href="/">Home</a>
         <a href="/search">Search</a>
-        <a href="/properties">Your Properties</a>
+        {isLandlord && <a href="/properties">Your Properties</a>}
         <a href="/all-properties">All Properties</a>
-        <a href="/add-property">Add Property</a>
-        <a href="/viewing-list">Viewing List</a>
-        <a href="/chat">Chat</a>
-        <a href="/calendar">Calendar</a>
-        <a href="/faq">FAQ Page</a>
+        {isLandlord && <a href="/add-property">Add Property</a>}
+        <a href="/viewing-list">Saved Properties</a>
+        {/* The rest of the menu items, if needed */}
       </div>
       <div className="logout-container">
         <a onClick={logoutUser}>Log Out</a>
@@ -40,7 +41,7 @@ const Sidebar = ({ user }) => {
 };
 
 Sidebar.propTypes = {
-  user: UserShape.isRequired
+  user: UserShape.isRequired,
 };
 
 export default Sidebar;
