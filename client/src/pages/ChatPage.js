41755b437a115ce215abb3ce9d05c8785053c38f
BrindaPragashkumar <ec21634@qmul.ac.uk>
2023-04-12 23:14:36 +0100

    Pages

---
+++ client/src/pages/ChatPage.js
@@ -0,0 +1,32 @@
+import React,{ useState, useEffect }  from 'react'
+import httpClient from '../../httpClient'
+import PropTypes from 'prop-types';
+import { UserShape } from "../../Types"
+import "../Pages.css"
+
+const ChatPage = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await httpClient.get('//localhost:5000/@me');
+
+        setUser(resp.data);
+      } catch (error) {
+        console.log('Not authenticated');
+      }
+    })();
+  }, []);
+
+  return (
+    <div className='main-content'>
+      ChatPage Hello World
+    </div>
+  )
+}
+ChatPage.propTypes = {
+  user: PropTypes.oneOfType([UserShape, PropTypes.instanceOf(null)]),
+};
+
+export default ChatPage
